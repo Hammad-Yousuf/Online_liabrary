0254d9894bc8515b3c65b34b67209066070b95b4
M. Hammad Yousuf <74192966+Hammad-Yousuf@users.noreply.github.com>
2021-08-09 11:49:52 +0500

    Add files via upload

--- JS/All_books.js
+++ JS/All_books.js
@@ -3,6 +3,60 @@ let all_books = all_books_container.books;
 let all_books_length = all_books.length;
 console.log(all_books_length);
 
+// All books
+
+for (let i = 0; i <= all_books_length - 1; i++) {
+  // Book Container
+  let book = document.createElement("div");
+  book.setAttribute("class", "book");
+  book.setAttribute("id", "book");
+
+  // Book Image
+  let book_img = document.createElement("img");
+  book_img.setAttribute("src", all_books[i].front_src);
+  book.appendChild(book_img);
+  all_container.appendChild(book);
+
+  // Hover Layer
+  let hover = document.createElement("div");
+  hover.setAttribute("class", "hover_layer");
+  book.appendChild(hover);
+
+  // If Volumes Availabe
+
+  if (all_books[i].volume_available == true) {
+    let volume_link_container = document.createElement("div");
+    let volume_link = document.createElement("a");
+    volume_link.setAttribute("class", "volume_link");
+    volume_link.setAttribute("href", all_books[i].volume_link);
+    volume_link_container.appendChild(volume_link);
+    let volume_link_text = document.createTextNode(
+      "Click Here For More Volumes!"
+    );
+    volume_link.appendChild(volume_link_text);
+    hover.appendChild(volume_link_container);
+  } else {
+    let volume_link_container = document.createElement("div");
+    let volume_link_text = document.createTextNode(" ");
+    volume_link_container.appendChild(volume_link_text);
+    hover.appendChild(volume_link_container);
+  }
+
+  // Download_btn
+
+  let download_btn = document.createElement("button");
+  download_btn.setAttribute("class", "download_btn");
+  let download_icon = document.createElement("i");
+  download_icon.setAttribute("class", "fad fa-download");
+  let download_link = document.createElement("a");
+  // download_link.src = "hello";
+  download_link.setAttribute("href", all_books[i].src);
+  // download_link.setAttribute('download', link.all[i].name)
+  download_link.appendChild(download_icon);
+  download_btn.appendChild(download_link);
+  hover.appendChild(download_btn);
+}
+
 // Search
 
 let user_choice = document.getElementById("categories").value;
--- JS/All_books_container.js
+++ JS/All_books_container.js
@@ -36,43 +36,116 @@ let all_books_array = `{
             "front_src": "./Images/Books Front/Islaah/Fazail e Amaal/Fazail e Amaal.PNG"
         },
         {
-            "name": "Fatawa E Usmani",
-            "author": "Mufti Taqi Usmani Saheb D.B",
-            "category": "fatwa",
-            "volume_available": true,
-            "volume_link": "https://drive.google.com/drive/folders/18scTXdnurmyW2UFP048xyiIbMqaeUHG8?usp=sharing",
-            "src": "https://drive.google.com/file/d/1KA0nfr9tMEN4yiSfS3RySC9_j4Pb09ue/view?usp=sharing",
-            "front_src": "./Images/Books Front/Fatawah/Fatawa e Usmani - Mufti Taqi Usmani Saheb D.B/Fatawa e Usmani 1.PNG"
-        },
-        {
-            "name": "Maine Kabul Baste Dekha",
-            "author": "Muhammad Maqsood Ahmed",
-            "category": "history",
+            "name": "Me Kab Aur Kis Tarah Ghar Se Bahar Ja Sakti Hu",
+            "author": "Mufti Amjad Saheb D.B",
+            "category": "islaah",
             "volume_available": false,
-            "src": "https://drive.google.com/file/d/1hiTZqDUZkourb8SlNMnj6FOPPe8ZKYbB/view?usp=sharing",
-            "front_src": "./Images/Books Front/History/Maine Kabul Baste Dekha/Maine Kabul Baste Dekha.PNG"
+            "src": "https://drive.google.com/file/d/1OfnVD4ocDTaNdRFT80ulAKjObuEpjoSc/view?usp=sharing",
+            "front_src": "./Images/Books Front/Islaah/Me Kab Aur Kis Tarah Ghar Se Bahar Ja Sakti Hu/Me Kab Aur Kis Tarah Ghar Se Bahar Ja Sakti Hu - Mufti Amjad Saheb.PNG"
+         },
+        {
+            "name": "Taba e Manzil Sirf Deewane Gaye",
+            "author": "Peer Zulfiqar Ahmed Naqshbandi Saheb D.B",
+            "category": "islaah",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1JLmcxP3oXSo1G9-sDV2vxVMRYA06LLQ8/view?usp=sharing",
+            "front_src": "./Images/Books Front/Islaah/Taba E Manzil Sirf Deewane Gaye/Taba E Manzil Sirf Deewane Gaye - Peer Zulfiqar Ahmed Naqshbandi.PNG"
         },
         {
-            "name": "Adaab Ul Muashrat",
+            "name": "Ayena e Muhabbat",
+            "author": "Hazrat Molana Shah Hakeem Muhammad Akhter Saheb R.A",
+            "category": "poetry",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1_eClE8YCY4IGHBYbhN77ttWFoj1ailX7/view?usp=sharing",
+            "front_src": "./Images/Books Front/Poetry/Ayena e Muhabbat/Ayena e Muhabbat.PNG"
+        },
+        {
+            "name": "Kuliyat e Akbar Ilahabadi",
+            "author": "Akbar Illahabadi",
+            "category": "poetry",
+            "volume_available": true,
+            "volume_link": "https://drive.google.com/drive/folders/10oR4d6ojGpy5q-G_YyhGeLDjHT11fzs-?usp=sharing",
+            "src": "https://drive.google.com/file/d/17h3_GexFI2yeN0uG0x-tcOBYQLx-fw2C/view?usp=sharing",
+            "front_src": "./Images/Books Front/Poetry/Deewan e Akbar/Deewan e Akbar.PNG"
+        },
+        {
+            "name": "Faizan e Muhabbat",
+            "author": "Hazrat Molana Shah Hakeem Muhammad Akhter Saheb R.A",
+            "category": "poetry",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1ywzDpXGlN6Aw27A6WDtlFvZXjrQiWKk8/view?usp=sharing",
+            "front_src": "./Images/Books Front/Poetry/Faizan e Muhabbat/Faizan e Muhabbat.PNG"
+        },
+        {
+            "name": "Kuliyat e Iqbal",
+            "author": "Allama Muhammad Iqbal R.A",
+            "category": "poetry",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1vvjZS_ECadnAHsVK2espJ_1KRjBqrzRo/view?usp=sharing",
+            "front_src": "./Images/Books Front/Poetry/Kuliyat e Iqbal/Kuliyat e Iqbal.PNG"
+        },
+        {
+            "name": "Adam A.S se Muhammad S.A.W tak",
+            "author": "Mufti Rafi Usmani Saheb D.B",
+            "category": "seerat un nabi",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1CWWiFVSPoc_upfYx6vHDyOgsmCJ006l2/view?usp=sharing",
+            "front_src": "./Images/Books Front/Seerat un Nabi/Adam A.S se Muhammad S.A.W tak/Adam A.S se Muhammad S.A.W tak.PNG"
+        },
+        {
+            "name": "Duniya Ki Haqeeqat",
+            "author": "Hazrat Molana Shah Hakeem Muhammad Akhter Saheb R.A",
+            "category": "seerat un nabi",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1M1jGUHS4rZx8MZlfixd8T8hF1UFYZUNP/view?usp=sharing",
+            "front_src": "./Images/Books Front/Seerat un Nabi/Duniya Ki Haqeeqat/Duniya Ki Haqeeqat.PNG"
+        },
+        {
+            "name": "Ehd e Nubuwat ke mah o saal",
+            "author": "Molana Muhammad Yousuf Ludhyanwi Shaheed R.A",
+            "category": "seerat un nabi",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1km8cqXWA30b9EeGd1CdkNzEXVsaLQ8MF/view?usp=sharing",
+            "front_src": "./Images/Books Front/Seerat un Nabi/Ehd e Nubuwat k mah o saal/Ehd e Nubuwat Ke Maah O Saal.PNG"
+        },
+        {
+            "name": "Seerat e Khatim ul Ambia S.A.W",
+            "author": "Mufti Muhammad Shafi Usmani Saheb R.A",
+            "category": "seerat un nabi",
+            "volume_available": false,
+            "src": "https://drive.google.com/file/d/1MuXDKi3LW4ChUWNRU2mLN1HPyik7hC4U/view?usp=sharing",
+            "front_src": "./Images/Books Front/Seerat un Nabi/Seerat e Khatim ul Ambia S.A.W/Seerat e Khatim ul Ambia S.A.W.PNG"
+        },
+        {
+            "name": "Seerat e Mustaf S.A.W",
+            "author": "Molana Muhammad Idrees Kandhalwi Saheb R.A",
+            "category": "seerat un nabi",
+            "volume_available": true,
+            "volume_link": "https://drive.google.com/drive/folders/1auS5eA6jE49Gmg6dhgAsd2vGno5k4QFM?usp=sharing",
+            "src": "https://drive.google.com/file/d/{1LPBYHgaDWtjzmGFS-MPiIJrJSCEntXyQ/view?usp=sharing",
+            "front_src": "./Images/Books Front/Seerat un Nabi/Seerat ul Mustafa S.A.W/Seerat ul Mustafa S.A.W.PNG"
+        },
+        {
+            "name": "Ashraf Ut Tafseer",
             "author": "Hazrat Molana Ashraf Ali Thanvi Saheb R.A",
-            "category": "islaah",
+            "category": "tafseer",
             "volume_available": true,
-            "volume_link": "https://drive.google.com/drive/folders/1nMp2lolNX5Ff-D4XsME4t1o1alvF9RIw?usp=sharing",
-            "src": "https://drive.google.com/file/d/11GYwJGMKlqjkg_Z9Im_s5eTDxRwzoq5D/view?usp=sharing",
-            "front_src": "./Images/Books Front/Islaah/Aadaab e Muashrat/Adaab E Muashrat - Molana Ashraf Ali Thanvi Saheb R.A.png"
+            "volume_link": "https://drive.google.com/drive/folders/1i9C-zqPc4gKGADnzlgpOCIRFSjlTWyMH?usp=sharing",
+            "src": "https://drive.google.com/file/d/1D6mqAno5oguWK3u8s_mcvrk3z4QW1H_d/view?usp=sharing",
+            "front_src": "./Images/Books Front/Tafseer/Ashraf Ut Tafseer/Ashraf Ut Tafseer.PNG"
         },
         {
-            "name": "Fazail e Amaal",
-            "author": "Molana Muhammad Zakariya Kandhalwi R.A",
-            "category": "islaah",
+            "name": "Duroos ul Quran",
+            "author": "Molana Ilyas Ghumman Saheb D.B",
+            "category": "tafseer",
             "volume_available": true,
-            "volume_link": "https://drive.google.com/drive/folders/19fuGKRCvX2Nb-zS9rOA58sG9B-pTSJMy?usp=sharing",
-            "src": "https://drive.google.com/file/d/1nGZkdmQX37KDeApRTy5ksw1PCMmfFuVh/view?usp=sharing",
-            "front_src": "./Images/Books Front/Islaah/Fazail e Amaal/Fazail e Amaal.PNG"
+            "volume_link": "https://drive.google.com/drive/folders/1nBTzCEYlxlcCBUEreK2NtXFBKFm-M_ET?usp=sharing",
+            "src": "https://drive.google.com/file/d/11uGvQMktaF4RVG8NeF_uR4-7yeuBIfNG/view?usp=sharing",
+            "front_src": "./Images/Books Front/Tafseer/Duroos Ul Quran/Duroos Ul Quran.PNG"
         }
     ]
-}
-`
+}`;
+
 // {
 //     "name": "",
 //     "author": "",
